961016f013f7ce2bb641b1b762574688b21e8471
Andydev0 <uvvanderson@gmail.com>
2025-04-30 22:55:14 -0300

    liberando cors para testar front-end

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS habilitado para testar no frontend 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  // Validação habilitada com whitelist
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(3000);
 }
 bootstrap();
